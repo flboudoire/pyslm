a9138453d035c0641a70d7115a6ad0c62f9f686b
Luke Parry <dev@lukeparry.uk>
2020-08-11 22:03:48 +0100

    Further development of the support generation example to include point and an attempt at edge support identification
    Signed-off-by: Luke Parry <dev@lukeparry.uk>

--- examples/support_generation.py
+++ examples/support_generation.py
@@ -48,6 +48,40 @@ def getUpskinMesh(part: Part, overhangAngle: float, splitMesh = False) -> trimes
     else:
         return overhangMesh
 
+def test(myPart: Part, overhangAngle: float, splitMesh=False) -> trimesh.Trimesh:
+    """
+    Gets the overhang mesh from a :class:`Part`. If the individual regions for the overhang mesh require separating,
+    the parameter :code:`splitMesh` should be set to True. This will split regions by their network connectivity using
+    Trimesh.
+
+    :param part: The part to extract the overhang mesh from
+    :param overhangAngle: The overhang angle in degrees
+    :param: splitMesh: If the overhang mesh should be split into separate trimesh entities by network connnectivity
+    :return: The overhang mesh
+    """
+
+    myPart.geometry.face_adjacency_edges
+    edgeIds = myPart.geometry.face_adjacency_edges
+
+
+    # Upward vector for support angles
+    v0 = np.array([[0., 0., -1.0]])
+
+    # Identify Support Angles
+    v1 = part.geometry.face_normals
+    theta = np.arccos(np.clip(np.dot(v0, v1.T), -1.0, 1.0))
+    theta = np.degrees(theta).flatten()
+
+    supportFaceIds = np.argwhere(theta > 95).flatten()
+
+    overhangMesh = trimesh.Trimesh(vertices=part.geometry.vertices,
+                                   faces=part.geometry.faces[supportFaceIds])
+
+    if splitMesh:
+        return overhangMesh.split(only_watertight=False)
+    else:
+        return overhangMesh
+
 
 def extrudeFace(supportFaceMesh, height=None, heightArray=None):
     # Locate boundary nodes/edges of the support face
@@ -122,8 +156,8 @@ def extrudeFace(supportFaceMesh, height=None, heightArray=None):
 ## CONSTANTS ####
 CORK_PATH = '/home/lparry/Development/src/external/cork/bin/cork'
 
-OVERHANG_ANGLE = 50 # deg - Overhang angle
-MIN_AREA_THRESHOLD = 0.5 # mm2
+OVERHANG_ANGLE = 45 # deg - Overhang angle
+MIN_AREA_THRESHOLD = 10 # mm2 (default = 10)
 RAY_PROJECTION_RESOLUTION = 0.2 #mm (default = 0.5)
 GRAD_THRESHOLD = 1.1* np.tan(np.deg2rad(OVERHANG_ANGLE)) * RAY_PROJECTION_RESOLUTION # DEFUALT = 0.5
 SUPPORT_EDGE_GAP = 0.5  # mm  - offset between part supports and baseplate supports (default = 1.0)
@@ -136,8 +170,11 @@ TRIANGULATION_SPACING = 0.5 # default = 1
 
 myPart = Part('myPart')
 myPart.setGeometry("../models/frameGuide.stl")
-myPart.rotation = [60.0, 50.0, -40.0]
-myPart.dropToPlatform()
+#myPart.rotation = [60.0, 50.0, -40.0]
+myPart.rotation = [45, 5.0, 0.0]
+myPart.dropToPlatform(20)
+
+
 
 """ Extract the overhang mesh - don't explicitly split the mesh"""
 overhangMesh = pyslm.support.getOverhangMesh(myPart, OVERHANG_ANGLE, False)
@@ -148,7 +185,107 @@ upskinMesh.visual.face_colors = [0.0, 1., 0., 1.0]
 
 # split the mesh
 overhangSubregions = overhangMesh.split(only_watertight=False)
+myPart.geometry.process(validate=True)
+#trimesh.constants.tol.merge = 1
+#myPart.geometry.merge_vertices()
+vAdjacency = myPart.geometry.vertex_neighbors
 
+edges = myPart.geometry.edges_unique
+meshVerts =  myPart.geometry.vertices
+centroids = myPart.geometry.triangles_center
+
+# Calculate the vertical face angles
+v0 = np.array([[0., 0., 1.0]])
+
+# Identify Support Angles
+v1 = myPart.geometry.face_normals
+theta = np.arccos(np.clip(np.dot(v0, v1.T), -1.0, 1.0))
+theta = np.degrees(theta).flatten()
+adjacentFaceAngles = np.rad2deg(myPart.geometry.face_adjacency_angles)
+
+
+overhangEdges = []
+overhangEdgeList = []
+edgeVerts = meshVerts[edges]
+
+rays = []
+
+for i in range(len(edgeVerts)):
+    edge = edgeVerts[i].reshape(2,3)
+    delta = edge[0] - edge[1]
+
+    mag = np.sqrt(np.sum(delta * delta))
+    ang = np.rad2deg(np.arcsin(delta[2]/mag))
+
+    if(np.abs(ang) < 10):
+        adjacentFaces = myPart.geometry.face_adjacency[i]
+        triVerts = meshVerts[myPart.geometry.faces[adjacentFaces]].reshape(-1, 3)
+
+        if adjacentFaceAngles[i] > OVERHANG_ANGLE and np.all(theta[adjacentFaces] > 89):
+            #if np.all(theta[adjacentFaces] > 89) and np.all(triVerts[:,2] - np.min(edge[:,2]) > -0.01):
+            overhangEdges.append(edges[i])
+            rays.append(edge)
+
+rays = np.array(rays)
+visualize_support_edges = trimesh.load_path((rays).reshape(-1, 2, 3))
+
+
+
+
+pointOverhang = []
+for i in range(len(vAdjacency)):
+    v = meshVerts[i]
+    verts = meshVerts[vAdjacency[i],:]
+    delta = verts-v
+    #mag = np.sqrt(np.sum(delta * delta, axis=1))
+    #theta = np.arcsin(delta[:,2]/mag)
+    #theta = np.rad2deg(theta)
+    #if np.all(theta > -0.001):
+        #pointOverhang.append(i)
+
+    if np.all(delta[:,2] > -0.05):
+        pointOverhang.append(i)
+
+if overhangEdges:
+    overhangEdges = np.array(overhangEdges)
+    overhangEdges = np.sort(overhangEdges, axis=1)
+    overhangEdges = np.unique(overhangEdges, axis=0)
+pointSupports =[]
+
+"""
+Generate the geometry for the supports
+"""
+rays = []
+for pnt in pointOverhang:
+    coords = np.zeros((2,3))
+    coords[:,:] = meshVerts[pnt]
+    coords[1,2] = 0.0
+    rays.append(coords)
+
+supportExtrudes = []
+for edge in overhangEdges:
+    coords = np.vstack((meshVerts[edge,:],meshVerts[edge,:]))
+    coords[2:,2] = 0.0
+
+    extrudeFace= np.array([(0, 1, 3), (3, 2, 0)])
+    supportExtrudes.append(trimesh.Trimesh(vertices=coords, faces=extrudeFace))
+
+rays = np.array(rays)
+ray_visualize = trimesh.load_path((rays).reshape(-1, 2, 3))
+
+myPart.geometry.visual.vertex_colors = [80,80,80, 125]
+s2 = trimesh.Scene([myPart.geometry, ray_visualize, visualize_support_edges, overhangMesh] + supportExtrudes)
+s2.show()
+
+
+d
+
+myPart.geometry.visual.vertex_colors = [50,50,50,125]
+
+myPart.geometry.visual.vertex_colors[pointOverhang] = [255, 255, 0.0, 255]
+
+myPart.geometry.show()
+de
 rays = []
 myExtrusions = []
 
@@ -221,7 +358,7 @@ for subregion in overhangSubregions:
     # Find the first location of any triangles which intersect with the part
     hitLoc, index_ray, index_tri = supportRegion.ray.intersects_location(ray_origins=coords,
                                                                          ray_directions=rays,
-                                                                         multiple_hits=False)
+                                                                         multiple_hits=True)
     print('\tfinished projecting rays')
 
     hitLocCpy = hitLoc.copy()
@@ -312,10 +449,11 @@ for subregion in overhangSubregions:
         else:
             polygons.append(bufferPoly)
 
-    plt.figure()
-    plt.imshow(heightMap.T)
-    for outline in outlines:
-        plt.plot(outline[:, 0], outline[:, 1])
+    if False:
+        plt.figure()
+        plt.imshow(heightMap.T)
+        for outline in outlines:
+            plt.plot(outline[:, 0], outline[:, 1])
     for bufferPoly in polygons:
 
         if bufferPoly.area < MIN_AREA_THRESHOLD:
@@ -388,7 +526,76 @@ overhangMesh.vertices[:,2] -= 0.1
 #myPart.geometry
 s = trimesh.Scene([overhangMesh,upskinMesh] + myExtrusions)
 # open the scene viewer and move a ball around
-s.show(viewer='gl')
+#s.show(viewer='gl')
+
 
 #overhangMesh.export('testSupport.ply');
-myPart.draw()
+#myPart.draw()
+
+supportGeom = myExtrusions[0]
+bx = supportGeom.bounds[:,0]
+by = supportGeom.bounds[:,1]
+
+spacingX = 3.0
+spacingY = 3.0
+
+# Obtain the section through the STL extension using Trimesh Algorithm (Shapely)
+sectionsX = supportGeom.section_multiplane(plane_origin=[0.0, 0, 0],
+                                                     plane_normal=[1, 0, 0],
+                                                     heights = np.arange(bx[0], bx[1], spacingX))
+
+sectionsY = supportGeom.section_multiplane(plane_origin=[0, 0.0, 0],
+                                                     plane_normal=[0.0, 1.0, 0],
+                                                     heights = np.arange(by[0], by[1], spacingY))
+
+xSectionMesh = trimesh.Trimesh()
+ySectionMesh = trimesh.Trimesh()
+
+for section in sectionsX:
+    if section is None:
+        continue
+
+    vx,fx = section.triangulate()
+    if(len(vx)  == 0):
+        continue
+
+
+    vx = np.insert(vx, 2, values=0.0, axis=1)
+    secX = trimesh.Trimesh(vertices=vx, faces=fx)
+    secX.apply_transform(section.metadata['to_3D'])
+    xSectionMesh += secX
+
+for section in sectionsY:
+
+    if section is None:
+        continue
+
+
+    vy,fy = section.triangulate()
+
+    if (len(vy) == 0):
+        continue
+
+    vy = np.insert(vy, 2, values=0.0, axis=1)
+    secY = trimesh.Trimesh(vertices=vy, faces=fy)
+    secY.apply_transform(section.metadata['to_3D'])
+    ySectionMesh += secY
+
+
+print('\t - start intersecting mesh')
+# Intersect the projection of the support face with the original part using the Cork Library
+xSectionMesh.export('secX.off')
+ySectionMesh.export('secY.off')
+subprocess.call([CORK_PATH, '-resolve', 'secY.off', 'secX.off', 'merge.off'])
+print('\t - finished intersecting mesh')
+isectMesh = trimesh.load_mesh('merge.off')
+#sectionsX[0].show()
+#sectionsY[0].show()
+
+supportGeom.visual.face_colors = [0.0, 1., 0., 0.3]
+s2 = trimesh.Scene([isectMesh, supportGeom])
+s2.show()
+
+# Obtain the 2D Planar Section at this Z-position
+#planarSection, transform = sections.to_planar(transformMat, normal=[1,0,0])
+
